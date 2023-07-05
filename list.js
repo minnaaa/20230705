c916134c2a0559788fe4749e434f1735e69368b3
thsalsdk <minah1475@naver.com>
2023-07-05 16:03:57 +0900

    0705수업끝

--- list.js
+++ list.js
@@ -27,3 +27,65 @@ async function fetch(pageno=1, pagesize=10){
         return null;
     }
 }
+
+function printContacts(contacts) {
+    const $parent = $('#tbody');
+    for(c of contacts) {
+      const html = `
+        <tr>
+          <td>${c.no}</td>
+          <td><a href='read.html?no=${c.no}'>${c.name}</a></td>
+          <td>${c.tel}</td>
+          <td>${c.address}</td>
+        </tr>
+      `;
+      $parent.append(html);
+    }
+  }
+
+  //pagination에 필요한 prev start end next pegeno를 리턴하는 함수
+  //getPagination(result)-> result에서 pageno pagesize totalcount를 꺼내는문법
+  //->구조분해할당
+  function getPagination({pageno, pagesize, totalcount, blockSize=5}) {
+    //페이지 갯수계산
+    const countOfPage = Math.ceil(totalcount/pagesize);
+    //p s e n 계산 후 목록의 끝에 도달하면 e n를 변경
+    const prev = Math.floor((pageno-1)/blockSize)*blockSize;
+    const start = prev+1;
+    let end = prev + blockSize;
+    let next = end + 1;
+    if(end>=countOfPage) {
+      end = countOfPage;
+      next = 0;
+    }
+    //객체를 변수로 분해 변수를 모아서 객체를 생성
+    //->구조분해할당
+    //return {prev:prev, start:start, end:end, next:next}과 같음
+    return {prev, start, end, next, pageno};
+  }
+
+  function printPagination({prev, start, end, next}, pageno) {
+    const $parent = $('#pagination')
+    if(prev>0) {
+      const html =`
+        <li class='page-item'>
+          <a class='page-link' href='list.html?pageno=${prev}'>이전으로</a>
+        </li>`;
+      $parent.append(html);
+    }
+    for(let i=start; i<=end; i++) {
+      const className = pageno===i? 'page-item active' : 'page-item';
+      const html =`
+        <li class='${className}'>
+          <a class='page-link' href='list.html?pageno=${i}'>${i}</a>
+        </li>`;
+      $parent.append(html);  
+    }
+    if(next>0) {
+      const html =`
+        <li class='page-item'>
+          <a class='page-link' href='list.html?pageno=${next}'>다음으로</a>
+        </li>`;
+      $parent.append(html);
+    }
+  }
